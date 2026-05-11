57bb49b5756a791f1cc2b3e46fa5843e8effedab
Simon <simonotaru14@gmail.com>
2026-05-11 17:23:14 +0100

    Update hero page

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { TbTopologyStar3 } from "react-icons/tb";
+import hero_img from "../../images/hero_img.webp";
 
 const Hero = () => {
   return (
@@ -16,7 +18,36 @@ const Hero = () => {
       ></div>
 
       <div className="relative z-10 mt-16 px-4 md:px-6 lg:px-6 xl:px-10 max-w-[90rem] mx-auto">
-        <div>ffff</div>
+        <div className="flex items-center justify-between gap-x-10">
+          {/* ------- hero left -------------- */}
+          <div className="flex flex-col ">
+            {/* -------------next gen star ---------------- */}
+            <div className="flex items-center gap-[8px] bg-[#b5d9cd] py-1 px-3 rounded-full text-[12px] w-fit">
+              <TbTopologyStar3 className="" />
+              <p className="font-light ">Next-Gen Career Intelligence</p>
+            </div>
+
+            {/* --------------- title --------------- */}
+            <div>
+              <h2 className="text-[60px] font-semibold  ">
+                Build a Resume That Lands the Job in{" "}
+                <span className="text-[var(--color-primary)]">Minutes</span>
+              </h2>
+              <p className="text-[18px] text-muted-foreground mt-4">
+                Create a standout resume in minutes with our AI-powered platform
+              </p>
+            </div>
+          </div>
+
+          {/* ------------ hero right images -------------- */}
+          <div>
+            <img
+              src={hero_img}
+              alt="An hero image of resume"
+              className="w-[1200px]"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
