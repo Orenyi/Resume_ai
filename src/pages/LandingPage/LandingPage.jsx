0b4fb43dd9686102c0b23c363769c61ea3ca7266
Simon <simonotaru14@gmail.com>
2026-05-12 10:56:22 +0100

    Results section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Results from "./Results";
 
 const LandingPage = () => {
   return (
     <section>
       <Hero />
+      <Results />
     </section>
   );
 };
---
+++ src/pages/LandingPage/Results.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Results = () => {
+  return (
+    <section className=" bg-[#f2f4f6] py-16">
+      <div className="px-4 md:px-6 lg:px-6 xl:px-10 max-w-[90rem] mx-auto">
+        <h2 className="text-[25px] md:text-[30px] lg:text-[40px] font-semibold leading-snug text-center">
+          Engineered for Results
+        </h2>
+        <p className="text-[16px] md:text-[18px] font-light text-muted-foreground mt-4 text-center max-w-2xl mx-auto">
+          Our specialized AI tools handle the complex parts of resume writing so
+          you can focus on your next big opportunity.
+        </p>
+      </div>
+
+      {/* ---------- grid of 4 ---------------- */}
+
+      
+    </section>
+  );
+};
+
+export default Results;
