a882c76d829792ffaf55a0b178afc4bd3cb86c2b
Simon <simonotaru14@gmail.com>
2026-05-22 14:02:31 +0100

    update template page

--- src/pages/Templates/Templates.jsx
+++ src/pages/Templates/Templates.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
+import Topbar from "../../components/Topbar";
+import useDashboardStore from "../../store/dashboardStore";
 
 const Template = () => {
+  const { sidebarOpen } = useDashboardStore();
   return (
     <section className="min-h-screen bg-[#f8fafc]">
       <Sidebar />
+      <main
+        className={`transition-all duration-300 px-4 md:px-6 lg:px-8 py-6
+          ${sidebarOpen ? "lg:ml-[260px]" : "lg:ml-[85px]"}`}
+      >
+        <div className="max-w-[85rem] mx-auto">
+          <Topbar />
+        </div>
+      </main>
     </section>
   );
 };
